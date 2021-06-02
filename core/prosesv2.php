27c34f41530bc5271dc3014be855775b9a1d1469
mrizkyff <muhamadrizkyff@gmail.com>
2021-06-02 12:19:04 +0700

    dapat 2 node

--- core/prosesv2.php
+++ core/prosesv2.php
@@ -47,6 +47,59 @@ $root = cari_root($pre_node);
 
 
 $pre_node = filter_node($pre_node, $root);
+print_r($root);
+foreach ($root as $key => $value) {
+    $root[$key]['lesser'] = '';
+    $root[$key]['greater'] = '';
+    $max_lesser_puas = $value['max_lesser_puas'];
+    $max_lesser_tidak = $value['max_lesser_tidak'];
+    $max_greater_puas = $value['max_greater_puas'];
+    $max_greater_tidak = $value['max_greater_tidak'];
+    if($max_lesser_puas == 0 and $max_lesser_tidak != 0){
+        $root[$key]['lesser'] = 'tidak';
+    }
+    else if($max_lesser_tidak == 0 and $max_lesser_puas != 0){
+        $root[$key]['lesser'] = 'puas';
+    }
+    if($max_greater_puas == 0 and $max_greater_tidak != 0){
+        $root[$key]['greater'] = 'tidak';
+    }
+    else if($max_greater_tidak == 0 and $max_greater_puas != 0){
+        $root[$key]['greater'] = 'puas';
+    }
+}
+// print_r($root);
+
+// $tangible = mapping_atribut($result, 'tangible','responsiveness',3.3,'lesser');
+
+foreach ($pre_node as $key => $value) {
+    foreach ($root as $key1 => $value1) {
+        $ambang = $value1['idx'];
+        $operator = '';
+        if($value1['greater'] == "puas"){
+            $operator = "lesser";
+        }
+        else if($value1['lesser'] == "puas"){
+            $operator = "greater";
+        }
+        $pre_node[$key] = mapping_atribut($result, $key, $key1, $ambang, $operator);
+    }
+}
+
+// cari nilai terbesar dari setiap atribut
+foreach ($pre_node as $key => $value) {
+    $pre_node[$key] = hitung_gain_ratio($pre_node[$key], $jml_data_puas, $jml_data_tidak);
+}
+
+// cari nilai terbesar dari seluruh atribut
+foreach ($pre_node as $key => $value) {
+    $pre_node[$key] = cari_max_gain_ratio($pre_node[$key]);
+}
+
+// print_r($pre_node);
+
+$root = cari_root($pre_node);
+
 // print_r($root);
 foreach ($root as $key => $value) {
     $root[$key]['lesser'] = '';
@@ -68,29 +121,9 @@ foreach ($root as $key => $value) {
         $root[$key]['greater'] = 'puas';
     }
 }
+// cek nilai lesser dan greaternya
+
 print_r($root);
-
-$tangible = mapping_atribut($result, 'tangible','responsiveness',3.3,'lesser');
-
-foreach ($pre_node as $key => $value) {
-    foreach ($root as $key1 => $value1) {
-        $ambang = $value1['idx'];
-        $operator = '';
-        if($value1['greater'] == "puas"){
-            $operator = "lesser";
-        }
-        else if($value1['lesser'] == "puas"){
-            $operator = "greater";
-        }
-        $pre_node[$key] = mapping_atribut($result, $key, $key1, $ambang, $operator);
-    }
-}
-
-foreach ($pre_node as $key => $value) {
-    $pre_node[$key] = hitung_gain_ratio($pre_node[$key], $jml_data_puas, $jml_data_tidak);
-}
-print_r($pre_node);
-
 
 
 
